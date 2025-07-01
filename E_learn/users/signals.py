86054ccb0492a40ed8f595f21e94ee6568eb0ae0
sidram24 <siddugowda1412@gmail.com>
2025-07-01 15:54:55 +0530

    email verification and otp validation

--- E_learn/users/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from .models import User
-
-@receiver(user_logged_in,sender=User)
-def cancel_deactivation_on_login(sender,request,user,**kwargs):
-    if user.profile.deactivated_at:
-        user.profile.deactivated_at=None
-        user.profile.save()
