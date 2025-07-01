fd17341f8368542733ab7c3e0b864ce9c4770047
sidram24 <siddugowda1412@gmail.com>
2025-07-01 17:43:07 +0530

    loaded data

--- havelse/build.sh
+++ havelse/build.sh
@@ -3,4 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py flush
 python manage.py loaddata initial_data.json
